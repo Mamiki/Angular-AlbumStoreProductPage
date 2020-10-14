cd44c7f184d16bb36cd089931d5918dd7852e1bb
unknown <mamiki85@gmail.com>
2020-10-14 06:23:00 +0100

    fourth challenge

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -12,10 +12,8 @@ export class ProductService {
 
   private  _albumUrl = '../assets/album.json';
   constructor(private _http: Http) { }
-  getAlbum(id: number) {
-    return this._http.get(this._albumUrl).map((response) => response.json());
-    return this.Observable<Album>;
-
+  getAlbum(id: number): Observable<Album> {
+    return this._http.get(this._albumUrl).map((response) => <Album>response.json());
     }
   }
 
